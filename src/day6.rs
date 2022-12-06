ce5de481b51d8e5cb542cc380a3b05ad3ddbcfd1
David Čadež <david.cadez7@gmail.com>
2022-12-07 00:35:33 +0100

    day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,42 +1,34 @@
-use std::cmp;
-use std::collections::VecDeque;
+use std::cmp::max;
 use std::fs;
 
 pub(crate) fn day6() -> (isize, isize) {
     let file_path = "data/day6.in";
     let contents = fs::read_to_string(file_path).expect("Wheres the file??");
 
-    let mut len = 4;
-    let (_, _, sum1) = contents
+    let mut l = 4;
+    let (_, sum1) = contents
         .chars()
-        .try_fold((VecDeque::new(), 0, 0), |(mut q, n, pos), c| {
-            if pos > len - 1 {
-                q.pop_back();
-            };
-            q.push_front(c);
-            match (q.iter().skip(1).position(|&x| x == c), n, pos > len - 1) {
-                (None, 1, true) => Err((q, n, pos + 1)),
-                (Some(m), _, _) => Ok((q, cmp::max(len - m, n) - 1, pos + 1)),
-                (None, _, _) => Ok((q, cmp::max(1, n) - 1, pos + 1)),
+        .try_fold((l, 0), |(n, pos), c| {
+            match (contents[max(l, pos) - l..pos].chars().rev().position(|x| x == c), n) {
+                (Some(m), n) => Ok((max(l, n + m - 1) - m, pos + 1)),
+                (None, 1) => Err((0, pos)),
+                (None, n) => Ok((max(1, n) - 1, pos + 1)),
             }
         })
-        .unwrap_or_else(|v| v);
+        .unwrap_or_else(|x| x);
 
-    len = 14;
-    let (_, _, sum2) = contents
+    l = 14;
+    let (_, sum2) = contents
         .chars()
-        .try_fold((VecDeque::new(), 0, 0), |(mut q, n, pos), c| {
-            if pos > len - 1 {
-                q.pop_back();
-            };
-            q.push_front(c);
-            match (q.iter().skip(1).position(|&x| x == c), n, pos > len - 1) {
-                (None, 1, true) => Err((q, n, pos + 1)),
-                (Some(m), _, _) => Ok((q, cmp::max(len - m, n) - 1, pos + 1)),
-                (None, _, _) => Ok((q, cmp::max(1, n) - 1, pos + 1)),
+        .try_fold((l, 0), |(n, pos), c| {
+            match (
+                contents[max(l, pos) - l..pos].chars().rev().position(|x| x == c), n) {
+                (Some(m), n) => Ok((max(l, n + m - 1) - m, pos + 1)),
+                (None, 1) => Err((0, pos)),
+                (None, n) => Ok((max(1, n) - 1, pos + 1)),
             }
         })
-        .unwrap_or_else(|v| v);
+        .unwrap_or_else(|x| x);
 
     (sum1.try_into().unwrap(), sum2.try_into().unwrap())
 }
