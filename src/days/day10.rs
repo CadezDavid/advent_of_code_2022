c270c391fc2e3d4d2536b1a52c7045eeb71b1959
David Čadež <david.cadez7@gmail.com>
2022-12-10 15:18:22 +0100

    day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,4 +1,32 @@
 use crate::Solution;
-pub fn solve(_input: &str) -> Solution {
-    todo!();
+pub fn solve(input: &str) -> Solution {
+    let cmds = input
+        .lines()
+        .flat_map(|line| match line.split_once(' ') {
+            Some((_, val)) => vec![0, val.parse().unwrap()],
+            None => vec![0],
+        })
+        .enumerate();
+
+    let (sum1, _) = &cmds.clone().fold((0, 1), |(sum, x), (i, n)| {
+        if (i + 21) % 40 == 0 && (i + 21) / 40 <= 6 {
+            (sum + (i as isize + 1) * x, x + n)
+        } else {
+            (sum, x + n)
+        }
+    });
+
+    let _ = &cmds.clone().fold(1, |sprite, (i, n)| {
+        if (sprite - (i as isize) % 40).abs() <= 1 {
+            print!("#");
+        } else {
+            print!(".");
+        }
+        if (i + 1) % 40 == 0 {
+            print!("\n");
+        }
+        sprite + n
+    });
+
+    Solution::Isize(*sum1, 0)
 }
--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,14 +1,4 @@
 use crate::Solution;
 pub fn solve(_input: &str) -> Solution {
-    // let forest: Vec<Vec<isize>> = input
-    //     .lines()
-    //     .map(|x| {
-    //         x.chars()
-    //             .map(|c| c.to_digit(10).expect("thats not a number") as isize)
-    //             .collect()
-    //     })
-    //     .collect();
-
-    // Solution::Isize(0, 0)
     todo!();
 }
