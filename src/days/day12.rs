7324c7c0846d7227b69d5230cf8906a32f3934db
David Čadež <david.cadez7@gmail.com>
2022-12-13 01:52:19 +0100

    day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,4 +1,74 @@
 use crate::Solution;
-pub fn solve(_input: &str) -> Solution {
-    todo!();
+use std::collections::VecDeque;
+pub fn solve(input: &str) -> Solution {
+    let mut area: Vec<char> = input.lines().flat_map(|line| line.chars()).collect();
+
+    let w = input.chars().position(|x| x == '\n').unwrap() as isize;
+    let h = input.chars().filter(|x| *x == '\n').count() as isize;
+
+    let start = area.iter().position(|c| *c == 'S').unwrap();
+    let end = area.iter().position(|c| *c == 'E').unwrap();
+    area[start] = 'a';
+    area[end] = 'z';
+
+    let directions: Vec<isize> = vec![-w, w, -1, 1];
+
+    fn bfs(graph: &Vec<Vec<usize>>, start: usize, end: Vec<usize>) -> usize {
+        let mut todo = VecDeque::new();
+        let mut dist: Vec<Option<usize>> = vec![None; graph.len()];
+        todo.push_back(start);
+        dist[start] = Some(0);
+        let mut curr = start;
+        while !todo.is_empty() {
+            curr = todo.pop_front().unwrap();
+            if end.contains(&curr) {
+                break;
+            }
+            for n in graph[curr].iter() {
+                if dist[*n] == None {
+                    todo.push_back(*n);
+                    dist[*n] = Some(dist[curr].unwrap() + 1);
+                }
+            }
+        }
+        dist[curr].unwrap()
+    }
+
+    let graph1 = (0..area.len())
+        .map(|i| {
+            directions
+                .iter()
+                .map(|di| i as isize + *di)
+                .filter(|j| {
+                    0 <= *j && *j < h * w && area[i] as isize >= area[*j as usize] as isize - 1
+                })
+                .map(|j| j as usize)
+                .collect()
+        })
+        .collect();
+
+    let graph2 = (0..area.len())
+        .map(|i| {
+            directions
+                .iter()
+                .map(|di| i as isize + *di)
+                .filter(|j| {
+                    0 <= *j && *j < h * w && area[i] as isize <= area[*j as usize] as isize + 1
+                })
+                .map(|j| j as usize)
+                .collect()
+        })
+        .collect();
+
+    let sum1 = bfs(&graph1, start, vec![end]);
+    let sum2 = bfs(
+        &graph2,
+        end,
+        area.iter()
+            .enumerate()
+            .filter_map(|(i, c)| if *c == 'a' { Some(i) } else { None })
+            .collect(),
+    );
+
+    Solution::Isize(sum1 as isize, sum2 as isize)
 }
--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,4 +1,62 @@
 use crate::Solution;
-pub fn solve(_input: &str) -> Solution {
-    todo!();
+pub fn solve(input: &str) -> Solution {
+    let forest: Vec<Vec<isize>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as isize)
+                .collect()
+        })
+        .collect();
+    let mut sum = 2 * (forest.len() + forest[0].len() - 2);
+    for x in 1..(forest.len() - 1) {
+        for y in 1..(forest[0].len() - 1) {
+            let m1 = forest[x][..y].iter().max().unwrap();
+            let m2 = forest[x][y + 1..].iter().max().unwrap();
+            let m3 = (0..x).map(|i| forest[i][y]).max().unwrap();
+            let m4 = (x + 1..forest.len()).map(|i| forest[i][y]).max().unwrap();
+            if forest[x][y] > *m1 || forest[x][y] > *m2 || forest[x][y] > m3 || forest[x][y] > m4 {
+                sum += 1;
+            }
+        }
+    }
+
+    let mut max = 0;
+    for x in 1..(forest.len() - 1) {
+        for y in 1..(forest[0].len() - 1) {
+            let tree = forest[x][y];
+            let m1 = forest[x][..y]
+                .iter()
+                .rev()
+                .try_fold(0, |n, t| if tree <= *t { Err(n + 1) } else { Ok(n + 1) })
+                .unwrap_or_else(|x| x);
+            let m2 = forest[x][y + 1..]
+                .iter()
+                .try_fold(0, |n, t| if tree <= *t { Err(n + 1) } else { Ok(n + 1) })
+                .unwrap_or_else(|x| x);
+            let m3 = (0..x)
+                .rev()
+                .map(|i| forest[i][y])
+                .try_fold(0, |n, t| {
+                    if tree <= t as isize {
+                        Err(n + 1)
+                    } else {
+                        Ok(n + 1)
+                    }
+                })
+                .unwrap_or_else(|x| x);
+            let m4 = (x + 1..forest.len())
+                .map(|i| forest[i][y])
+                .try_fold(0, |n, t| {
+                    if tree <= t as isize {
+                        Err(n + 1)
+                    } else {
+                        Ok(n + 1)
+                    }
+                })
+                .unwrap_or_else(|x| x);
+            max = max.max(m1 * m2 * m3 * m4);
+        }
+    }
+    Solution::Isize(sum as isize, max)
 }
